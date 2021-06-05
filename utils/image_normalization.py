12c7a044eaf526db18597c1e58be2dcd37204839
adrofa <artem.drofa@gmail.com>
2021-06-05 21:50:07 +0300

    crossval_split corrected

--- utils/image_normalization.py
+++ utils/image_normalization.py
@@ -1,4 +1,4 @@
-from utils.support import pkl_load, jsn_dump
+from utils.support import pkl_load, jsn_dump, get_logger
 
 from pathlib import Path
 import os
@@ -8,7 +8,7 @@ from tqdm import tqdm
 import sys
 
 
-def main(cfg):
+def main(cfg, logger):
     results_dir = Path(cfg["output"]) / "image_normalization"
     os.makedirs(results_dir, exist_ok=True)
     jsn_dump(cfg, results_dir / "config.json")
@@ -21,29 +21,32 @@ def main(cfg):
     count = 0
     for img_path in tqdm(img_paths, file=sys.stdout, total=len(img_paths)):
         img = cv2.imread(str(img_path), cv2.IMREAD_GRAYSCALE)
-        if img is not None:
-            img = img / 255
-            sum_ += img.sum()
-            count += img.shape[0] * img.shape[1]
+        img = img / 255
+        sum_ += img.sum()
+        count += img.shape[0] * img.shape[1]
     mean = np.round(sum_ / count, 3)
+    logger.info("Mean is collected.")
 
     # std
     diff_squared = 0
     for img_path in tqdm(img_paths, file=sys.stdout, total=len(img_paths)):
         img = cv2.imread(str(img_path), cv2.IMREAD_GRAYSCALE)
-        if img is not None:
-            img = img / 255
-            diff_squared += ((img - mean) ** 2).sum()
+        img = img / 255
+        diff_squared += ((img - mean) ** 2).sum()
     std = np.round(np.sqrt(diff_squared / count), 3)
+    logger.info("Std is collected.")
 
     with open(results_dir / "normalization_params.txt", "w") as file:
         file.write(f"mean = {mean}")
         file.write("\n")
         file.write(f"std = {std}")
+    logger.info("Results dumped.")
+
+    logger.info(f"Mean: {mean} | Std: {std}.")
 
 
 if __name__ == "__main__":
     config = {
         "output": "../output"
     }
-    main(config)
+    main(config, get_logger("image_normalization"))
